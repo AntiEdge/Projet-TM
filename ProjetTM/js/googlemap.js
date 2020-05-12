2d443b59cb8554ec8e38ce02b7f1421faaabd38b
Falcon <misterT9412@gmail.com>
2020-05-13 01:58:24 +0200

    Map working

--- ProjetTM/js/googlemap.js
+++ ProjetTM/js/googlemap.js
@@ -81,7 +81,7 @@ function codeAddress(vdata) {
 // a la fonction action.php qui inscrit les éléments dans la base
 function updateCityWithLatLng(points) {
 	$.ajax({
-		url:"action.php",
+		url:"../includes/action.php",
 		method:"post",
 		data: points,
 		success: function(res) {
@@ -90,3 +90,131 @@ function updateCityWithLatLng(points) {
 	})
 
 }
+
+
+
+
+
+
+
+/*
+
+//Trouve un ensemble de résultat en fonction de la position actuelle.
+function findResults(lat, lng, numberHits=20){
+
+    index.search('', {
+
+        aroundLatLng: lat + ", " + lng,
+        // aroundRadius: 5000,
+        hitsPerPage: numberHits
+      }).then(({ hits }) => {
+        searchList.clear()
+        clearMarkers()
+        hits.map(createMarker)
+        setBounds()
+        $( ".list li:first-of-type" ).trigger("click") //open infowindow for closest element
+      });
+}
+
+function runWithGeolocation(position) {
+    mapCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    // map.setCenter(mapCenter);
+    // map.setZoom(10);
+    findResults(position.coords.latitude, position.coords.longitude);
+
+    geocoder.geocode({'location': mapCenter}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            $('#zipCode').val(results[0].formatted_address).parent().addClass('is-dirty');
+        } else {
+            console.log("Geocode was not successful for the following reason: ")
+        }
+    });
+}
+
+function init() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(runWithGeolocation);
+    } else {
+      console.log("Geolocation is not supported by this browser.")
+    }
+}
+
+$( document ).ready(function() {
+    //codeAddress('Mons');
+    init()
+});
+
+
+//                                           Test Search
+
+
+
+
+//ALGOLIA SETUP
+const client = algoliasearch('JWHPBFC4T1', '6eb371014c3bff23b98dde01a8ef1763');
+const index = client.initIndex('prod_schools');
+
+var listTemplate = `<li>
+    <a href="#" class="shadow-sm list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1 siteName"></h5>
+            <small class="distance"></small>
+        </div>
+        <p class="mb-1 collapsed"></p>
+    <small></small>
+</a></li>
+`
+
+
+var directionsIcon = `<svg class="bi bi-arrow-90deg-right" width="1.3rem" height="1.3rem" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M9.896 2.396a.5.5 0 000 .708l2.647 2.646-2.647 2.646a.5.5 0 10.708.708l3-3a.5.5 0 000-.708l-3-3a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M13.25 5.75a.5.5 0 00-.5-.5h-6.5a2.5 2.5 0 00-2.5 2.5v5.5a.5.5 0 001 0v-5.5a1.5 1.5 0 011.5-1.5h6.5a.5.5 0 00.5-.5z" clip-rule="evenodd"/>
+</svg>`
+
+var searchOptions = {
+    valueNames: [ 'siteName', 'siteAddress', 'distance'],
+    item: listTemplate
+};
+
+var searchList = new List('searchList', searchOptions);
+
+
+
+function findResults(lat, lng, numberHits=20){
+
+    index.search('', {
+
+        aroundLatLng: lat + ", " + lng,
+        // aroundRadius: 5000,
+        hitsPerPage: numberHits
+      }).then(({ hits }) => {
+        searchList.clear()
+        clearMarkers()
+        hits.map(createMarker)
+        setBounds()
+        $( ".list li:first-of-type" ).trigger("click") //open infowindow for closest element
+      });
+}
+
+function distance(mk1, mk2) {
+    var R = 3958.8; // Radius of the Earth in miles
+    var rlat1 = mk1.lat() * (Math.PI/180); // Convert degrees to radians
+    var rlat2 = mk2.lat() * (Math.PI/180); // Convert degrees to radians
+    var difflat = rlat2-rlat1; // Radian difference (latitudes)
+    var difflon = (mk2.lng()-mk1.lng()) * (Math.PI/180); // Radian difference (longitudes)
+
+    var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+    var roundedDistance = (Math.round(d * 10) / 10).toFixed(1);
+    var string = roundedDistance.toString() + ' miles away';
+    return string;
+}
+
+function setBounds(){
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markerArray.length; i++) {
+        bounds.extend(markerArray[i].getPosition());
+    }
+    map.fitBounds(bounds);
+}
+*/
